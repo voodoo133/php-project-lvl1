792ed2abac643066eaf425b69c8bf6c4b969f4d8
Anton Okhrimenko <voodoo_133@mail.ru>
2020-03-09 14:19:08 +0300

    Made one more part of the project

---
+++ src/Cli.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function run ()
+{
+  line("Welcome to the Brain Games!");
+  $name = prompt("May I have your name?");
+  line("Hello, %s!", $name);
+}
+
+
+
