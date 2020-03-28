ffd1f6b7cf971ce8057d737458178f33e01bab51
Anton Okhrimenko <voodoo_133@mail.ru>
2020-03-28 17:04:45 +0300

    Added CodeClimate

--- src/Cli.php
+++ src/Cli.php
@@ -5,12 +5,9 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function run ()
+function run()
 {
-  line("Welcome to the Brain Games!");
-  $name = prompt("May I have your name?");
-  line("Hello, %s!", $name);
+    line("Welcome to the Brain Games!");
+    $name = prompt("May I have your name?");
+    line("Hello, %s!", $name);
 }
-
-
-
