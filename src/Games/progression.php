e4706af9f0b7657e42cb71e2c93ce00b2ac7c1d6
Anton Okhrimenko <voodoo_133@mail.ru>
2020-06-13 11:11:07 +0300

    Added game

---
+++ src/Games/progression.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BrainGames\Games\progression;
+
+use function BrainGames\game\startGame;
+
+DEFINE('PROGRESSION_LENGTH', 10);
+
+function run()
+{
+    $start_msg = "What number is missing in the progression?";
+
+    $get_game_data = function () {
+        $initial_num = mt_rand(1, 10);
+        $step = mt_rand(1, 5);
+
+        $progression = [$initial_num];
+
+        for ($i = 1; $i < PROGRESSION_LENGTH; $i++) {
+            $progression[] = $progression[$i - 1] + $step;
+        }
+
+        $correct_answer_key = mt_rand(1, count($progression));
+        $correct_answer = $progression[$correct_answer_key];
+        $progression[$correct_answer_key] = '..';
+
+        $question = join(" ", $progression);
+
+        return [$question, $correct_answer];
+    };
+
+    startGame($start_msg, $get_game_data);
+}
